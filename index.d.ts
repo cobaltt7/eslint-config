e5ac00ff4b519838a2a8879026e8f5b5056e23a5
cobalt <61329810+cobaltt7@users.noreply.github.com>
2025-05-01 19:33:50 -0500

    Update typings
    Signed-off-by: cobalt <61329810+cobaltt7@users.noreply.github.com>

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,21 @@
-import type { config, ConfigArray } from "typescript-eslint";
+import type { Linter } from "eslint";
+import type { config } from "typescript-eslint";
 
+import { defineConfig } from "eslint/config";
 import globals from "globals";
 
-declare const configArray: ConfigArray;
+declare const configType: Linter.Config;
 
+/** @deprecated Use {@link defineConfig} */
 export declare const declareConfig: typeof config;
 export { globals };
 
 export namespace configs {
-	export { configArray as global };
-	export { configArray as configs };
-	export { configArray as declarations };
-	export { configArray as tests };
+	export { configType as global };
+	export { configType as configs };
+	export { configType as declarations };
+	export { configType as tests };
 }
 
-declare const _default: ConfigArray[];
+declare const _default: Linter.Config[];
 export default _default;
