30d0ec7f594d88ea1e0b4eff71e702a12ded3cc5
cobalt <61329810+cobaltt7@users.noreply.github.com>
2025-08-06 16:24:26 -0500

    Add node globals to config files by default
    Signed-off-by: cobalt <61329810+cobaltt7@users.noreply.github.com>

--- configs.js
+++ configs.js
@@ -1,8 +1,10 @@
+import globals from "globals";
 import typescriptEslint from "typescript-eslint";
 
 export default typescriptEslint.config({
 	name: "cobaltt7/configs",
 	files: ["**/*.config.js"],
+	languageOptions: { globals: globals.nodeBuiltin },
 	rules: {
 		"id-match": "off",
 		"sort-keys": ["error", "asc", { caseSensitive: false, natural: true }],
